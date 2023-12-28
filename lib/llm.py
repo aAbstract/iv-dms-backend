bed0a6e01a544ea69f4fe1b26b4d5ca9abd09b67
Eslam Elsharkawy <abstract@pop-os.localdomain>
2023-12-28 19:19:31 +0200

    marked recursive constraints audit as optional

--- lib/llm.py
+++ lib/llm.py
@@ -21,6 +21,6 @@ async def iosa_audit_text(iosa_item: IOSAItem, text: str) -> ServiceResponse:
         score_tag=score,
         score_text=score_tags_text_map[score],
         description='some description',
-        details=[],  # details is computed recursively on constrains tree in iosa_item
+        details=[],  # [OPTIONAL] details is computed recursively on constrains tree in iosa_item
     )
     return ServiceResponse(data={'llm_resp': llm_resp})
