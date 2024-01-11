29e133e14ed368480a81a4fd279037bc7b387c49
MohamedElgemeie <mgalal2002@outlook.com>
2024-01-11 18:18:08 +0200

    Merge branch 'main' of https://github.com/aAbstract/iv-dms-backend

---
+++ scripts/mongo_setup.bat
@@ -0,0 +1,1 @@
+docker run -d --name mongo -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=example -v iv_dms_mdb_data:/data/db -v iv_dms_mdb_config:/data/configdb mongo
