c5b915dc2746363c060019e57074eed4a7b8d471
MohamedElgemeie <mgalal2002@outlook.com>
2024-02-23 01:41:47 +0200

    Update seed_schema.py

--- scripts/seed_schema.py
+++ scripts/seed_schema.py
@@ -582,7 +582,7 @@ def seed_routine():
         "nesma_ch15.pdf": "e1fb39f6-9c86-4b58-8ccb-0aebb1dbf075",
         "nesma_ch12.pdf": "79a57df5-5047-413f-9b88-68abc13b98a5",
     }
-    for file_path in glob(r"data\nesma_OMA\*.pdf"):
+    for file_path in glob(r"data/nesma_OMA/*.pdf"):
         filename = re.split(r"[\\|/]", file_path)[-1]
         traget_mde = [
             x for x in json_obj if x["filename"] == f"data/nesma_OMA/{filename}"
